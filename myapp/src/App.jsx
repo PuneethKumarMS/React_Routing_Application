0c58e1fdc489253ef79f811139d965aa6a15da53
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-01-20 21:52:24 +0530

    Update App.jsx

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+// importing files from component folder
 import Home from './components/Home';
 import Navbar from "./components/Navbar";
 import Contact from "./components/Contact";
